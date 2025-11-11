ffebc9f5e0b0daabebd5873e598196648b0b1735
Lucien Dilane <dilanelucien@gmail.com>
2025-11-11 07:56:14 +0100

    dernières retouches

--- centre/settings.py
+++ centre/settings.py
@@ -24,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-wszxzzdqsup#*wbsjnr&dj_iew&e%m5fe#ml2r+tmwu)v36-p@'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
 DEBUG = config('DEBUG',default=True,cast=bool)
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost', config('ALLOWED_HOSTS', default='')] 
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', *config('ALLOWED_HOSTS', default='').split(',')]
 SECRET_KEY = config('SECRET_KEY')
 
 
